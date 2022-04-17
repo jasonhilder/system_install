240b7658bc617efb8ed2bab2f3021350c8ff2509
Jason Hilder <jason@pop-os.localdomain>
2022-04-17 21:53:30 +0200

    Added conditionals and user prompts.

--- theme.sh
+++ theme.sh
@@ -1,52 +1,95 @@
 #!/bin/bash
+echo "Install all options or select? [y/n]"
+echo ""
+echo "Note: If you type n (no) you will be asked before each install to proceed."
+
+read _override
+
+if [[ $_override = "y" ]]; then
+    _proceed="y"
+fi
 
 #MAKE DIRS FOR THEME
-mkdir ~/.icons
-mkdir ~/.themes
+if [[ ! -d "${HOME}/.icons" ]]; then
+    mkdir ~/.icons
+fi
+
+if [[ ! -d "${HOME}/.themes" ]]; then
+    mkdir ~/.themes
+fi
 
 #OH MY ZSH PLUGINS
-echo "installing zsh auto suggestion plugin..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "installing zsh syntax highlighting plugin..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
+    echo "installing zsh auto suggestion plugin..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-#HACK FONT
-font="Hack Nerd Font"
+if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]]; then
+    echo "installing zsh syntax highlighting plugin..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+#SPACE MONO FONT
+font="Space Mono Nerd Font"
 fc-list -q "$font"
 if [ $? -eq 0 ]
 then
-    echo "Hack Font found on system moving on..."
+    echo "Space Mono Nerd Font found on system moving on..."
 else
     echo "Adding font to system..."
 
-    echo "Downloading Hack Nerdfont..."
-    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip" -q --show-progress
+    echo "Downloading Space Mono Nerd Font..."
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SpaceMono.zip" -q --show-progress
 
     if [ -d $HOME"/.fonts/" ]; then
-        unzip -q "./Hack.zip" -d $HOME"/.fonts/"
+        unzip -q "./SpaceMono.zip" -d $HOME"/.fonts/"
     else
        mkdir -p $HOME"/.fonts/"
-       unzip -q "./Hack.zip" -d $HOME"/.fonts/"
+       unzip -q "./SpaceMono.zip" -d $HOME"/.fonts/"
     fi
 
     echo "Cleaning up download..."
-    rm -rf "./Hack.zip"
+    rm -rf "./SpaceMono.zip"
 fi
 
-echo -e "\n"
+#CATPPUCCIN GTK THEME
+if [[ ! $_override = "all" ]]; then
+    echo "Install catppucin blue theme? [y/n]"
+    read _proceed
+fi
 
-#GNOME TWEAKS
-if ! command -v gnome-tweaks &> /dev/null
-then
-    echo ###################
-    echo "Installing gnome tweaks..."
-    echo ###################
-    echo 
+if [[ $_proceed = "y" ]]; then
+    if [[ ! -d "${HOME}/.themes/Catppuccin-blue" ]]; then
+        tag=$(curl --silent https://api.github.com/repos/catppuccin/gtk/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
 
-    sudo apt install gnome-tweaks -y
+        echo $tag
+
+        VERSION=$(echo $tag|cut -c 2-6)
+        FILE="Catppuccin-blue.zip"
+
+        download="https://github.com/catppuccin/gtk/releases/download/"$tag"/"$FILE
+        cd ~ && wget -O $FILE $download -q --show-progress
+    else
+        echo "Theme already installed..."
+    fi
+else
+    echo "As you wish"
+fi
+
+#PAPIRUS ICONS
+if [[ ! $_override = "all" ]]; then
+    echo "Install Papirus Icon theme? [y/n]"
+    read _proceed
+fi
+
+if [[ $_proceed = "y" ]]; then
+    if [[ ! -d "${HOME}/.icons/Papirus" ]]; then
+        wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
+    else
+        echo "Icons already installed..."
+    fi
 fi
 
 #REFRESH ZSHRC && ALIASES
 cd ~ && rm .zshrc
 cd ~/.dotfiles && stow zsh/
-cd ~ && source ~/.zshrc && zshalias
